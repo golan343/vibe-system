72504c4c50516b53e716b6b49737634aa7e87d40
Golan Elitzur <59259952+golan343@users.noreply.github.com>
2022-10-19 17:57:46 +0300

    Update config.js

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,4 +1,4 @@
 
-export const BaseUrl = `https://vibe-server-version-1.herokuapp.com/api`;
-// export const BaseUrl = `http://localhost:3001/api`;
+
+ export const BaseUrl = `http://localhost:3001/api`;
 
